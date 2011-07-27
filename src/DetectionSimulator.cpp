eea09f7fc6e1dad444078bf5f9243ef106fd6447
Mike Krainin <mkrainin@willowgarage.com>
2011-07-27 14:58:00 -0700

    adding files

---
+++ src/DetectionSimulator.cpp
@@ -0,0 +1,72 @@
+/*
+ * DetectionSimulator.cpp
+ *
+ *  Created on: Jul 26, 2011
+ *      Author: mkrainin
+ */
+
+#include "ecto/ecto.hpp"
+#include "geometry_msgs/PoseStamped.h"
+
+namespace ecto_corrector
+{
+  using ecto::tendrils;
+
+  struct DetectionSimulator
+  {
+  public:
+    static void declare_params(tendrils& params)
+    {
+      params.declare<std::string>("ply", "PLY file w/ triangulated mesh");
+      params.declare<double>("x", "x component of the pose of the object wrt camera");
+      params.declare<double>("y", "y component of the pose of the object wrt camera");
+      params.declare<double>("z", "z component of the pose of the object wrt camera");
+      params.declare<double>("qx", "qx component of the pose of the object wrt camera");
+      params.declare<double>("qy", "qy component of the pose of the object wrt camera");
+      params.declare<double>("qz", "qz component of the pose of the object wrt camera");
+      params.declare<double>("qw", "qw component of the pose of the object wrt camera");
+    }
+
+    static void declare_io(const tendrils& /*params*/, tendrils& in, tendrils& out)
+    {
+      out.declare<std::string>("ply_file", "PLY file w/ triangulated mesh");
+      out.declare<geometry_msgs::PoseStamped>("output_pose", "Pose estimate");
+    }
+
+    void configure(tendrils& params, tendrils& /*in*/, tendrils& /*out*/)
+    {
+      ply=params.at("ply");
+      x=params.at("x");
+      y=params.at("y");
+      z=params.at("z");
+      qx=params.at("qx");
+      qy=params.at("qy");
+      qz=params.at("qz");
+      qw=params.at("qw");
+    }
+
+    int process(const tendrils& in, tendrils& out)
+    {
+      geometry_msgs::PoseStamped pose;
+      pose.header.stamp = ros::Time::now();
+      pose.header.frame_id = "/object";
+      pose.pose.position.x = x.read();
+      pose.pose.position.y = y.read();
+      pose.pose.position.z = z.read();
+      pose.pose.orientation.x = qx.read();
+      pose.pose.orientation.y = qy.read();
+      pose.pose.orientation.z = qz.read();
+      pose.pose.orientation.w = qw.read();
+
+      out.get<std::string>("ply_file") = ply.read();
+      out.get<geometry_msgs::PoseStamped>("output_pose") = pose;
+
+      return ecto::OK;
+    }
+
+  private:
+    ecto::spore<std::string> ply;
+    ecto::spore<double> x,y,z,qx,qy,qz,qw;
+  };
+} //namespace
+ECTO_CELL(ecto_corrector, ecto_corrector::DetectionSimulator, "DetectionSimulator", "Outputs ply and pose that were set manually");
---
+++ src/ecto_corrector.cpp
@@ -0,0 +1,4 @@
+#include <ecto/ecto.hpp>
+
+//set up the module
+ECTO_DEFINE_MODULE(ecto_corrector) {}
