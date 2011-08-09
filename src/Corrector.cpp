4fba61b24a316b869a95ec9762e31eb25603a933
Mike Krainin <mkrainin@willowgarage.com>
2011-08-09 11:47:14 -0700

    filled in most of Corrector cell

--- src/Corrector.cpp
+++ src/Corrector.cpp
@@ -11,6 +11,7 @@
 #include "pose_corrector/model/rigidObjectModel.h"
 
 #include "pcl/point_cloud.h"
+#include "pcl/io/io.h"
 
 namespace ecto_corrector
 {
@@ -44,18 +45,67 @@ namespace ecto_corrector
           "output_pose", "Refined object pose estimate");
     }
 
-    void configure(tendrils& params, tendrils& /*in*/, tendrils& /*out*/)
+    void configure(tendrils& params, tendrils& in, tendrils& out)
     {
-      //TODO
+      //inputs
+      in_pose_ = in["input_pose"];
+      model_ = in["model"];
+      cam_info_ = in["camera_info"];
+      depth_edges_ = in["depth_edges"];
+      cloud_ = in["cloud"];
+
+      //outputs
+      out_pose_ = out["output_pose"];
     }
 
     int process(const tendrils& in, tendrils& out)
     {
-      //TODO
+      //get initial pose
+      tf::Transform init_pose;
+      tf::poseMsgToTF(in_pose_->pose,init_pose);
+
+      //set up and run corrector
+      pose_corrector::Corrector corrector;
+      corrector.setModel(*model_);
+      corrector.setModelBasePose(init_pose);
+      corrector.initCamera(*cam_info_);
+
+      //put the cloud in the right format
+      pcl::PointCloud<PointT> const& cloud_in = **cloud_;
+      pcl::PointCloud<pcl::PointNormal> cloud;
+      // Allocate enough space and copy the basics
+      cloud.points.resize (cloud_in.points.size ());
+      cloud.header   = cloud_in.header;
+      cloud.width    = cloud_in.width;
+      cloud.height   = cloud_in.height;
+      cloud.is_dense = cloud_in.is_dense;
+      for (size_t i = 0; i < cloud_in.points.size (); ++i){
+        PointT const& pt = cloud_in.points[i];
+        cloud.points[i].x = pt.x;
+        cloud.points[i].y = pt.y;
+        cloud.points[i].z = pt.z;
+      }
+
+      //perform the correction
+      corrector.correct(cloud,*depth_edges_);
+
+      //set output pose
+      out_pose_->header = in_pose_->header;
+      tf::poseTFToMsg(corrector.getModelBasePose(),out_pose_->pose);
+
       return ecto::OK;
     }
 
   private:
+    //inputs
+    ecto::spore<geometry_msgs::PoseStamped> in_pose_;
+    ecto::spore<boost::shared_ptr<pose_corrector::RigidObjectModel> > model_;
+    ecto::spore<sensor_msgs::CameraInfoConstPtr> cam_info_;
+    ecto::spore<cv::Mat> depth_edges_;
+    ecto::spore<typename pcl::PointCloud<PointT>::ConstPtr > cloud_;
+
+    //outputs
+    ecto::spore<geometry_msgs::PoseStamped> out_pose_;
 
   };
 } //namespace
