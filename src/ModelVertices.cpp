faf26a29f55f64a8f2e6793eaac41ee0cb8b0d17
Mike Krainin <mkrainin@willowgarage.com>
2011-08-16 12:49:30 -0700

    changed ModelVertices to ouput a PointCloud2

--- src/ModelVertices.cpp
+++ src/ModelVertices.cpp
@@ -25,9 +25,9 @@ namespace ecto_corrector
 
     static void declare_io(const tendrils& /*params*/, tendrils& in, tendrils& out)
     {
-      in.declare<boost::shared_ptr<const pose_corrector::RigidObjectModel> >("model");
-      in.declare<geometry_msgs::PoseStamped>("pose");
-      out.declare<pcl::PointCloud<pcl::PointXYZ>::Ptr >("cloud");
+      in.declare<boost::shared_ptr<pose_corrector::RigidObjectModel> >("model", "Model whose vertices are to be extracted");
+      in.declare<geometry_msgs::PoseStamped>("pose", "Pose of model");
+      out.declare<sensor_msgs::PointCloud2ConstPtr>("cloud", "PointCloud2 with vertices");
     }
 
     void configure(const tendrils& params, const tendrils& in, const tendrils& out)
@@ -40,25 +40,29 @@ namespace ecto_corrector
     int process(const tendrils& in, const tendrils& out)
     {
       //get cloud
-      *cloud_ = (*model_)->getVertexCloud();
+      pcl::PointCloud<pcl::PointXYZ>::Ptr cloud = (*model_)->getVertexCloud();
 
       //transform
       geometry_msgs::Point t = pose_->pose.position;
       geometry_msgs::Quaternion q = pose_->pose.orientation;
-      pcl::transformPointCloud(**cloud_,**cloud_,Eigen::Vector3f(t.x,t.y,t.z),Eigen::Quaternionf(q.w,q.x,q.y,q.z));
+      pcl::transformPointCloud(*cloud,*cloud,Eigen::Vector3f(t.x,t.y,t.z),Eigen::Quaternionf(q.w,q.x,q.y,q.z));
 
-      (*cloud_)->header = pose_->header;
+      cloud->header = pose_->header;
+
+      sensor_msgs::PointCloud2Ptr cloud2(new sensor_msgs::PointCloud2());
+      pcl::toROSMsg(*cloud,*cloud2);
+      *cloud_ = cloud2;
 
       return ecto::OK;
     }
 
   private:
     //inputs
-    ecto::spore<boost::shared_ptr<const pose_corrector::RigidObjectModel> > model_;
+    ecto::spore<boost::shared_ptr<pose_corrector::RigidObjectModel> > model_;
     ecto::spore<geometry_msgs::PoseStamped> pose_;
 
     //outputs
-    ecto::spore<pcl::PointCloud<pcl::PointXYZ>::Ptr > cloud_;
+    ecto::spore<sensor_msgs::PointCloud2ConstPtr > cloud_;
 
   };
 } //namespace
